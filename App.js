5bee30fb7cdda4bcba68a874849b5926dd4b3846
James Priest <james-priest@outlook.com>
2019-05-02 03:19:07 -0700

    chore: Add ESLint &, Prettier

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    console.log('begin');
+    debugger;
+    console.log('end');
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text style={styles.redText}>abc</Text>
+        <Text style={styles.redText}>My red text works</Text>
+        <MaterialCommunityIcons name="emoticon-devil" color="red" size={101} />
       </View>
     );
   }
