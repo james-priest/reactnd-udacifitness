715e67ead27875f226b5b8d7c002c9c5b3cb2ab3
James Priest <james-priest@outlook.com>
2019-05-03 01:28:27 -0700

    feat: TouchableHighlight example

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback
+} from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
@@ -8,10 +16,19 @@ export default class App extends React.Component {
     debugger;
     console.log('end');
   }
+  handlePress = () => {
+    alert('hello!');
+  };
   render() {
     return (
       <View style={styles.container}>
-        <AddEntry />
+        <TouchableHighlight
+          style={styles.btn}
+          onPress={this.handlePress}
+          underlayColor="#d4271b"
+        >
+          <Text style={styles.btnText}>Touchable Highlight</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -21,7 +38,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginLeft: 10,
+    marginRight: 10,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  btnText: {
+    color: '#fff'
   }
 });
