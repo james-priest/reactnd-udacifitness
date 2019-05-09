74ca8ffe0202b1fe3dbd9842084ebede16799022
James Priest <james-priest@outlook.com>
2019-05-09 04:06:36 -0700

    chore: Add redux-logger

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AddEntry from './components/AddEntry';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import History from './components/History';
 
+const store = createStore(
+  reducer /* preloadedState, */,
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
+);
 export default class App extends React.Component {
   componentDidMount() {
     console.log('begin');
     debugger;
     console.log('end');
   }
-  handlePress = () => {
-    alert('hello!');
-  };
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider store={store}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          {/* <AddEntry /> */}
+          <History />
         </View>
       </Provider>
     );
