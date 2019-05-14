a59bfd6393809b6540030a542eb6203ed7997dd1
James Priest <james-priest@outlook.com>
2019-05-14 15:19:19 -0700

    feat: Add Android Material Tab Nav

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import {
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator
+} from 'react-navigation';
 import History from '../components/History';
 import AddEntry from '../components/AddEntry';
 import { purple, white } from '../utils/colors';
 
-export default createBottomTabNavigator(
+// export default
+const android = createMaterialTopTabNavigator(
   {
     History: History,
     AddEntry: AddEntry
   },
   {
+    navigationOptions: {
+      header: null
+    },
+    defaultNavigationOptions: {
+      bounces: true
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === 'ios' ? purple : white,
+      style: {
+        height: 70,
+        backgroundColor: Platform.OS === 'ios' ? white : purple,
+        shadowColor: 'rgba(0,0,0, 0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      },
+      tabStyle: {
+        marginTop: 5
+      },
+      showIcon: true
+    }
+  }
+);
+
+const ios = createBottomTabNavigator(
+  {
+    History: History,
+    AddEntry: AddEntry
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
     tabBarOptions: {
       activeTintColor: Platform.OS === 'ios' ? purple : white,
       style: {
@@ -27,3 +67,5 @@ export default createBottomTabNavigator(
     }
   }
 );
+
+export default (Platform.OS === 'ios' ? ios : android);
