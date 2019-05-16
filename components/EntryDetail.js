944139785bd1c6c4e647afe6d212d5957daaa0c9
James Priest <james-priest@outlook.com>
2019-05-16 03:13:28 -0700

    feat: Add MetricCard to EntryDetail screen

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { white } from '../utils/colors';
+import MetricCard from '../components/MetricCard';
 
 export class EntryDetail extends Component {
   static propTypes = {
-    navigation: PropTypes.object.isRequired
+    navigation: PropTypes.object.isRequired,
+    metrics: PropTypes.object.isRequired,
+    entryId: PropTypes.string.isRequired
   };
   static navigationOptions = ({ navigation }) => {
     const entryId = navigation.getParam('entryId', 'No Id');
@@ -20,18 +24,33 @@ export class EntryDetail extends Component {
   };
 
   render() {
+    const { metrics, entryId } = this.props;
+
     return (
-      <View>
-        <Text>Entry Detail</Text>
-        <Text>
-          EntryId: {this.props.navigation.getParam('entryId', 'No Id')}
-        </Text>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
+        <Text>EntryId: {entryId}</Text>
       </View>
     );
   }
 }
 
-const mapStateToProps = state => ({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15
+  }
+});
+
+const mapStateToProps = (state, { navigation }) => {
+  const entryId = navigation.getParam('entryId', 'No Id');
+
+  return {
+    entryId,
+    metrics: state[entryId]
+  };
+};
 
 const mapDispatchToProps = {};
 
